556f25ef7187528328543ef015cf09ff220f3353
gitYoungren <youngrengitonga@gmail.com>
2022-11-22 08:16:25 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your solution here!
+const cats=["Milo","Otis","Garfield","Ralph"];
+function Arrayfunction(cats){
+    function beforeEarth(){
+        cats.length=0;
+        cats.push('Milo','Otis','Garfield','Ralph');
+    }
+}
+function destructivelyAppendCat(cats){
+    function destructivelyAppendCat(Ralph);
+    cats.to.have.ordered.members('Milo','Otis ','Garfield','Ralph');
+}
