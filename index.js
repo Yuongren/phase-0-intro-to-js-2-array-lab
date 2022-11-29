0779b3a85f19fbf2a541d443418ef4564e556f22
gitYoungren <youngrengitonga@gmail.com>
2022-11-29 10:49:18 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,12 +1,63 @@
 // Write your solution here!
-const cats=["Milo","Otis","Garfield","Ralph"];
+const cats =["Milo","Otis","Garfield",];
 function Arrayfunction(cats){
     function beforeEarth(){
         cats.length=0;
         cats.push('Milo','Otis','Garfield','Ralph');
     }
 }
-function destructivelyAppendCat(cats){
-    function destructivelyAppendCat(Ralph);
-    cats.to.have.ordered.members('Milo','Otis ','Garfield','Ralph');
+function Arrayfunction(){
+    const cats = size ()
+    const size = cats.length
+    return size
+}
+print('Number of cats:', getNumberofcats)
+
+function  destructivelyAppendCat(){
+    //cats.push('Milo', 'Otis', 'Garfield');
+    cats.push('Ralph')
+    //cats.unshift('Bob')
+    //cats.pop('Garfield')
+    return cats
+}
+print(' destructively AppendCat',  destructivelyAppendCat)
+
+function destructivelyPrependCat(){
+    cats.unshift('Bob')
+    return cats
+}
+print(' destructively PrependCat',  destructivelyPrependCat)
+
+function destructivelyRemoveLastCat(){
+    cats.pop('Garfield')
+    return cats
+}
+print(' destructively RemoveLastCat',  destructivelyRemoveLastCat)
+
+function destructivelyRemoveFirstCat(){
+    cats.shift('Milo')
+    return cats
+}
+print(' destructively RemoveFirstCat',  destructivelyRemoveFirstCat)
+
+function appendCat(name){
+    let cats2 = [...cats, name]
+       return cats2
+}
+
+
+function prependCat(name){
+    let cats2 = [name, ...cats]
+       return cats2
+}
+
+
+function  removeLastCat(name){
+    let cats2 = cats.slice(0, -1)
+       return cats2
+}
+
+function removeFirstCat(){
+   let cats2 = cats.slice(1)
+    return cats2
 }
